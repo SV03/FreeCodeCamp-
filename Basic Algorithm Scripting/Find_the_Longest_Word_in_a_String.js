acdeca8c62eb7c214f24acb7c1ab20d136ee80d7
Sarath <Sarath.Vaman@city.ac.uk>
2018-02-28 01:18:02 +0000

    implement algorithm using for loop
    Implemented find the longest word in a string  function

--- Basic Algorithm Scripting/Find_the_Longest_Word_in_a_String.js
+++ Basic Algorithm Scripting/Find_the_Longest_Word_in_a_String.js
@@ -1,5 +1,15 @@
 function findLongestWord(str) {
-  return str.length;
+  var strSplit = str.split(" ");
+  
+  var longestWord = 0;
+  
+  for(var i = 0; i < strSplit.length; i++){
+    if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  
+  return longestWord;
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
