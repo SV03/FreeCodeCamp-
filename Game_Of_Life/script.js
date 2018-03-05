c05a2d6492b7c3e6607d54dbbbb1448d91773ad1
Sarath <Sarath.Vaman@city.ac.uk>
2018-03-05 13:59:17 +0000

    Create grid with rows and columns

--- Game_Of_Life/script.js
+++ Game_Of_Life/script.js
@@ -1,1 +1,22 @@
+var app = angular.module('gameOfLifeApp', []);
+app.controller('gameOfLifeCtrl', function($scope, $interval){
 
+  //grid
+  var rowMax = 25;
+  var colMax = 30;
+
+  //create grid
+  $scope.rows = [];
+  for(var i=0; i<rowMax; i++){
+    var col = [];
+    for (var j=0; j <colMax; j++){
+      var elem = {'active': 0};
+      col.push(elem);
+
+    }
+    $scope.rows.push({'cols':col});
+  }
+
+
+
+})
